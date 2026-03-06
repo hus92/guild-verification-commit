8cd3dad336c0f7ab6ae7fb06c52e647421c9bec2
hus92 <94604649+hus92@users.noreply.github.com>
2026-03-06 15:21:38 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T11:59:59.205Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-06T12:21:37.960Z
+Commit: Patch security vulnerability in authentication
